e86bd02c59b0777c7cc698901a78113aee784da6
manuferro <ferro1@libero.it>
2020-04-27 12:04:08 +0200

    Aggiungere i file di progetto.

---
+++ WpfCustomControlLibrary1/ClassTest01.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestLib01
+{
+    class ClassTest01
+    {
+        public int TestVersion { get => 1; }
+    }
+}
